b10c72fce9a02125054891d8fe6519ed8cdd2f87
Briana Coppard <bcoppard@zendesk.com>
2018-02-09 14:07:53 +1100

    add logging to bootstrap;

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
 # install xcode commandline tools
+echo "Installing Xcode Command Line Tools..."
 xcode-select --install
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+echo "Installing Homebrew..."
+if [ -z `command -v brew` ]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+fi;
 
 # install brew programs
-`pwd`/brew.sh
+echo "Installing Homebrew Packages..."
+#`pwd`/brew.sh
 
 # set zsh to default shell
-chsh -s $(which zsh)
+echo "Setting default shell to zsh..."
+#chsh -s $(which zsh)
 
 # symlink dotfiles
-ln -s ./symlink/zshrc ~/.zshrc
-ln -s ./symlink/zshrc.custom ~/.zshrc.custom
-ln -s ./symlink/zshrc.work ~/.zshrc.work
-ln -s ./symlink/vimrc.local ~/.vimrc.local
-ln -s ./symlink/gitignore ~/.gitignore
-ln -s ./symlink/gitconfig ~/.gitconfig
+echo "Symlinking dotfiles..."
+rm ~/.zshrc
+ln -s `pwd`/symlink/zshrc ~/.zshrc
+rm ~/.zshrc.custom
+ln -s `pwd`/symlink/zshrc.custom ~/.zshrc.custom
+rm ~/.zshrc.work
+ln -s `pwd`/symlink/zshrc.work ~/.zshrc.work
+rm ~/.vimrc.local
+ln -s `pwd`/symlink/vimrc.local ~/.vimrc.local
+rm ~/.gitignore
+ln -s `pwd`/symlink/gitignore ~/.gitignore
+rm ~/.gitconfig
+ln -s `pwd`/symlink/gitconfig ~/.gitconfig
